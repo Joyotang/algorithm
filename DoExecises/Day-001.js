0b049dcdaca01ba8f7b26b084532c7363f50e9ea
乐潇游 <tx@thinktxt.com>
2020-07-28 19:23:40 +0800

    add day002

--- DoExecises/Day-001.js
+++ DoExecises/Day-001.js
@@ -1,4 +1,22 @@
 /**
+ * 70.爬楼梯
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let dp = [];
+
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+       dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
+
+
+
+/**
  * 392. 判断子序列
  * @param {*} s 
  * @param {*} t 
---
+++ DoExecises/Day-002.js
@@ -0,0 +1,39 @@
+/**
+ * 66.加一
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let n = digits.length;
+    for (let i = n - 1; i >= 0; i--) {
+        digits[i]++;
+        digits[i] = digits[i] % 10;
+        if (digits[i] !== 0) {
+            return digits;
+        }
+    }
+    digits.unshift(1);
+    return digits;
+};
+
+
+
+/**
+ * 104.二叉树的最大深度
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    return Math.max(left, right) + 1;
+};
