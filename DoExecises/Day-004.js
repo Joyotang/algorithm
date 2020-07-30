5cfdc53acb6054679533f01a3dd0b1028c2e289a
乐潇游 <tx@thinktxt.com>
2020-07-30 20:20:35 +0800

    add day004

---
+++ DoExecises/Day-004.js
@@ -0,0 +1,15 @@
+/**
+ * 343. 整数拆分
+ * https://leetcode-cn.com/problems/integer-break/
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+    let max = 1;
+    if (n === 2 || n === 3) return n - 1;
+
+    for (; n > 4; n = n - 3) {
+        max = max * 3;
+    }
+    return max * n;
+};
