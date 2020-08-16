d4944f26939ac4a80592e2b33479daef07ba3e0c
TangXiao <tx@vimplus.com>
2020-08-16 22:48:40 +0800

    update

--- EverydayExecises/Day-020.js
+++ EverydayExecises/Day-020.js
@@ -71,3 +71,34 @@ var permuteUnique = function(nums) {
 
 
 console.log(permuteUnique([1,1,2]))
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var permuteUnique = function(nums) {
+    let n = nums.length;
+    nums.sort((a, b) => a - b);
+    let res = [];
+    let hash = {};
+
+    function backtrack(tempList) {
+        if (tempList.length === n) {
+            res.push([...tempList]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            // 剪枝
+            if (nums[i] || i > 0 && nums[i-1] === nums[i] && nums[i-1] === false) continue;
+
+            hash[nums[i]] = true;
+
+            tempList.push(nums[i]);
+            backtrack(tempList);
+            tempList.pop();
+
+            hash[nums[i]] = false;  
+        }
+    }
+    backtrack([])
+    return res;
+};
+
