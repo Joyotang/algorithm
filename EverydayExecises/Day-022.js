468e9aee8b17d90f5b3c5ee499ffbf6ad77d493c
TangXiao <tx@vimplus.com>
2020-08-17 22:17:26 +0800

    update

--- EverydayExecises/Day-022.js
+++ EverydayExecises/Day-022.js
@@ -36,3 +36,49 @@ var maxProfit = function(prices) {
     }
     return maxProfit;
 }
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 1. 自顶向下式
+ * 
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if (!root) return true;
+
+    function depth(root) {
+        if (!root) return 0;
+        let left = depth(root.left);
+        let right = depth(root.right);
+
+        return Math.max(left, right) + 1;;
+    }
+
+    return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+};
+
+
+// 2. 自底向顶式
+var isBalanced = function(root) {
+    return recursion(root) != -1;
+};
+
+function recursion(root) {
+    if (!root) return 0;
+    let left = recursion(root.left);
+    if (left === -1) return -1;
+    let right = recursion(root.right);
+    if (right === -1) return -1;
+
+    return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+}
