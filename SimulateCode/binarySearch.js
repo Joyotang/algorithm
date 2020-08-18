5cfa066bb236083d5d2ae7038896d2df5330e36e
TangXiao <tx@vimplus.com>
2020-08-18 23:10:42 +0800

    update

--- SimulateCode/binarySearch.js
+++ SimulateCode/binarySearch.js
@@ -1,9 +1,10 @@
 function binarySearch(array, target) {
     let left = 0;
     let right = array.length - 1;
-    let mid = (left + right) >> 1; // 相当于：mid = Math.floor(array.length / 2);
 
     while (left <= right) {
+        let mid = (left + right) >> 1; // 相当于：Math.floor((left + right) / 2);
+
         if (array[mid] === target) {
             return mid;
         } else if (array[mid] < target) {
