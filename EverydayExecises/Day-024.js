8ac47b2d0d5d5ededa4195406323f59fb257e0ce
乐潇游 <tx@thinktxt.com>
2020-08-19 19:41:25 +0800

    add day024

---
+++ EverydayExecises/Day-024.js
@@ -0,0 +1,40 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+
+};
+
+
+
+
+/**
+ * 647. 回文子串
+ * https://leetcode-cn.com/problems/palindromic-substrings/
+ * 
+ * 解法一：中心扩展法
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = (s) => {
+    let count = 0;
+    let n = s.length;
+
+    for (let i = 0; i < 2 * n - 1; i++) {
+        let start = Math.floor(i / 2);
+        let end = Math.floor(i / 2) + i % 2;
+        console.log('start:', start);
+        console.log('end:', end);
+        while (start >= 0 && end < n && s[start] === s[end]) {
+            console.log('-------------------')
+            start--;
+            end++;
+            count++;
+        }
+    }
+    return count;
+};
+
+
+console.log(countSubstrings("bbca"))
