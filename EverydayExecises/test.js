866c6250a9e579ae9ad70f5120f8b145022d1af7
乐潇游 <tx@thinktxt.com>
2020-08-27 19:41:19 +0800

    update

--- EverydayExecises/test.js
+++ EverydayExecises/test.js
@@ -1,27 +1,16 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var repeatedSubstringPattern = function(s) {
-    let n = s.length;
-
-    for (let i = 1; i * 2 <= n; i++) {
-        let match = true;
-        // 如果子集不是父集的倍数，那肯定不是重复的子字符串
-        if (n % i === 0) {
-            debugger
-            for (let j = i; j < n; j++) {
-                if (s[j] != s[j - i]) {
-                    match = false;
-                    break;
-                }
-            }
+var plusOne = function(digits) {
+    let n = digits.length;
+    for (let i = n - 1; i >= 0; i--) {
+        digits[i]++;
+        digits[i] = digits[i] % 10;
+        if (digits[i] !== 0) {
+            return digits;
         }
-        if (match) return true;
     }
-    return false;
+    digits.unshift(1);
+    return digits;
 };
 
 
 
-console.log(repeatedSubstringPattern('abab'))
+console.log(plusOne([8, 8]))
