bd8ab260d8dca5c0885cda80caa3a296fac7c6fa
TangXiao <tx@vimplus.com>
2020-08-29 20:15:25 +0800

    update

--- EverydayExecises/Day-004.js
+++ EverydayExecises/Day-004.js
@@ -14,12 +14,16 @@ var integerBreak = function(n) {
     return max * n;
 };
 
+
+
+/* -----------------------------------The next day coding.------------------------------------- */
 const integerBreak = function(n) {
-    let max = 1;
-    if (n === 2 || n === 3) return n - 1;
-    
-    for (; n > 4; n = n - 3) {
-        max = max * 3;
+    if (n <= 3) return n - 1;
+    let ans = 1;
+
+    while (n > 4) {
+        ans = ans * 3;
+        n = n - 3;
     }
-    return max * n;
+    return ans * n;
 }
