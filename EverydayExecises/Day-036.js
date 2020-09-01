c23f32e1aff775b63c6ead5fe58fdb74be2c4aa6
TangXiao <tx@vimplus.com>
2020-09-01 22:50:20 +0800

    update day036

--- EverydayExecises/Day-036.js
+++ EverydayExecises/Day-036.js
@@ -17,8 +17,30 @@ var canVisitAllRooms = function(rooms) {
 
         let cols = rooms[i];
         let len = cols.length;
-        for (let i = 0; i < len; i++) {
-            dfs(cols[i]);
+        for (let j = 0; j < len; j++) {
+            dfs(cols[j]);
+        }
+    }
+    dfs(0);
+    return count === rooms.length;
+};
+
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var canVisitAllRooms = function(rooms) {
+    let hash = new Map();
+    let count = 0;
+
+    function dfs(i) {
+        if (hash.has(i)) return;
+
+        count++;
+        hash.set(i, true);
+
+        let cols = rooms[i];
+        let len = cols.length;
+        for (let j = 0; j < len; j++) {
+            dfs(cols[j]);
         }
     }
     dfs(0);
