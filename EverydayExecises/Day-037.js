889568255b325924a1406ae4e221feda510a2292
乐潇游 <tx@thinktxt.com>
2020-09-01 19:50:57 +0800

    add day037

---
+++ EverydayExecises/Day-037.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var PredictTheWinner = function(nums) {
+    const len = nums.length;
+    const dp = new Array(len);
+
+    for (let i = 0; i < len; i++) {
+        dp[i] = new Array(len);
+    }
+    
+    for (let i = 0; i < len; i++) {
+        dp[i][i] = nums[i];
+    }
+
+    for (let i = len - 2; i >= 0; i--) {
+        for (let j = i + 1; j < len; j++) {
+            const pickI = nums[i] - dp[i + 1][j];
+            const pickJ = nums[j] - dp[i][j - 1];
+            dp[i][j] = Math.max(pickI, pickJ);
+        }
+    }
+    return dp[0][len - 1] >= 0;
+};
--- EverydayExecises/test.js
+++ EverydayExecises/test.js
@@ -1,21 +1,71 @@
-var repeatedSubstringPattern = function(s) {
-    let n = s.length;
-
-    for (let i = 1; i * 2 <= n; i++) {
-        // 如果子集不是父集的倍数，那肯定不是重复的子字符串
-        if (n % i === 0) {
-            let match = true;
-            for (let j = i; j < n; j++) {
-                debugger
-                if (s[j] != s[j - i]) {
-                    match = false;
-                    break;
-                }
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var minPathSum = function (grid) {
+    // 时间复杂度和空间复杂度都是 O (m * n);
+    if (grid.length === 0) return 0;
+    const dp = [];
+    const rows = grid.length;
+    const cols = grid[0].length;
+    // 实际上你也可以无差别全部填充为MAX_VALUE，对结果没影响,代码还会更少
+    // 只是有点不专业而已
+    for (let i = 0; i < rows + 1; i++) {
+        dp[i] = [];
+        // 初始化第一列
+        dp[i][0] = Number.MAX_SAFE_INTEGER;
+        for (let j = 0; j < cols + 1; j++) {
+            // 初始化第一行
+            if (i === 0) {
+                dp[i][j] = Number.MAX_SAFE_INTEGER;
             }
-            if (match) return true;
         }
     }
-    return false;
+    debugger
+
+    // tricky
+    // dp[0][1] = 0;
+
+    // for (let i = 1; i < rows + 1; i++) {
+    //     for (let j = 1; j < cols + 1; j++) {
+    //         // state transition
+    //         dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+    //     }
+    // }
+
+
+    
+
+    return dp[rows][cols];
 };
 
-console.log(repeatedSubstringPattern('aab'))
+
+function minPathSum(grid) {
+    if (grid === null || grid.length === 0 || grid[0].length === 0) return 0;
+    let rows= grid.length;
+    let cols= grid[0].length;
+    
+
+
+
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        return 0;
+    }
+    int rows = grid.length, columns = grid[0].length;
+    int[][] dp = new int[rows][columns];
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < rows; i++) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+    for (int j = 1; j < columns; j++) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+    }
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < columns; j++) {
+            dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        }
+    }
+    return dp[rows - 1][columns - 1];
+}
+
+console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
