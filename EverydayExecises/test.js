d193547e3ed945987865b74d0f9a8a7d15059de7
TangXiao <tx@vimplus.com>
2020-09-02 22:31:23 +0800

    add maxSubArray

--- EverydayExecises/test.js
+++ EverydayExecises/test.js
@@ -1,30 +1,16 @@
 /**
  * @param {number[]} nums
- * @return {boolean}
+ * @return {number}
  */
-function minPathSum(grid) {
-    if (grid === null || grid.length === 0 || grid[0].length === 0) return 0;
-    let rows = grid.length;
-    let cols = grid[0].length;
-    let dp = Array.from(new Array(rows), () => new Array(cols));
+var maxSubArray = function(nums) {
+    let n = nums.length;
+    if (!n) return 0;
 
-    dp[0][0] = grid[0][0];
-
-    for (let i = 1; i < rows; i++) {
-        dp[i][0] = dp[i - 1][0] + grid[i][0];
+    let dp = nums;
+    for (let i = 1; i < n; i++) {
+        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
     }
+    return Math.max(...dp);
+};
 
-    for (let j = 1; j < cols; j++) {
-        dp[0][j] = dp[0][j - 1] + grid[0][j];
-    }
-
-    for (let i = 1; i < rows; i++) {
-        for (let j = 1; j < cols; j++) {
-            dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-        }
-    }
-
-    return dp[rows - 1][cols - 1];
-}
-
-console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
