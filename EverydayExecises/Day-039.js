24c747b1e355305777ba2f0c795603976cb2a402
TangXiao <tx@vimplus.com>
2020-09-04 22:04:18 +0800

    update day039

--- EverydayExecises/Day-039.js
+++ EverydayExecises/Day-039.js
@@ -36,3 +36,17 @@ var maxSubArray = function(nums) {
     }
     return maxAns;
 }
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var maxSubArray = function(nums) {
+    let pre = 0;
+    let maxAns = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        pre = Math.max(pre + nums[i], nums[i]);
+        maxAns = Math.max(pre, maxAns);
+    }
+    return maxAns;
+}
+
+
