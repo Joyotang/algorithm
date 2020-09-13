3bb105a951763a0881f009d01b2deab41585f17a
TangXiao <tx@vimplus.com>
2020-09-13 23:08:37 +0800

    add day049 and update day048

--- EverydayExecises/Day-048.js
+++ EverydayExecises/Day-048.js
@@ -29,3 +29,22 @@ var averageOfLevels = function(root) {
     }
     return res;
 };
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var averageOfLevels = function(root) {
+    let res = [];
+    let queue = [root];
+
+    while (queue.length) {
+        const levelSize = queue.length;
+        let levelSum = 0;
+        for (let i = 0; i < levelSize; i++) {
+            let curNode = queue.shift();
+            levelSum += curNode.val;
+            if (curNode.left) queue.push(curNode.left);
+            if (curNode.right) queue.push(curNode.right);
+        }
+        res.push(levelSum / levelSize);
+    }
+    return res;
+};
