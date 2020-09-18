af37ed57609e1ee8698aa6d084cb567342c2de6f
TangXiao <tx@vimplus.com>
2020-09-18 22:45:55 +0800

    update day053

--- EverydayExecises/Day-053.js
+++ EverydayExecises/Day-053.js
@@ -1,4 +1,7 @@
 /**
+ * 56. 合并区间
+ * https://leetcode-cn.com/problems/merge-intervals/
+ * 
  * @param {number[][]} intervals
  * @return {number[][]}
  */
@@ -11,6 +14,25 @@ var merge = function(intervals) {
     for (let i = 1; i < intervals.length; i++) {
         if (intervals[i][0] <= res[res.length - 1][1]) {
             res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1]);
+        } else {
+            res.push(intervals[i]);
+        }
+    }
+    return res;
+};
+
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var merge = function(intervals) {
+    let res = [];
+    if (!intervals || !intervals.length) return res;
+
+    intervals.sort((a, b) => a[0] - b[0]);
+    res.push(intervals[0]);
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] <= res[res.length - 1][1]) {
+            res[res.length - 1][1] = Math.max(intervals[i][1], res[res.length - 1][1])
         } else {
             res.push(intervals[i]);
         }
