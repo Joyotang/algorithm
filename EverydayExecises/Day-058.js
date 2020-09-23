c8177cdecba72bf5676723d51dedc0f743fd5c93
乐潇游 <tx@thinktxt.com>
2020-09-23 17:08:19 +0800

    add day059 and update 058 & 042

--- EverydayExecises/Day-058.js
+++ EverydayExecises/Day-058.js
@@ -23,3 +23,24 @@ var uniquePaths = function(m, n) {
 };
 
 console.log(uniquePaths(3, 2))
+
+/* -----------------------------------The next day coding.------------------------------------- */
+const uniquePaths = (m, n) => {
+    let dp = Array.from(new Array(m), () => new Array(n));
+
+    for (let i = 0; i < m; i++) {
+        dp[i][0] = 1;
+    }
+
+    for (let j = 0; j < n; j++) {
+        dp[0][j] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
