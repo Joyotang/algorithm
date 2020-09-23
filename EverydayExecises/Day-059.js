fa08ae6ec6a8a21ec90e1774efb00e0ea7e6a11d
TangXiao <tx@vimplus.com>
2020-09-23 22:51:35 +0800

    add day059

--- EverydayExecises/Day-059.js
+++ EverydayExecises/Day-059.js
@@ -18,3 +18,72 @@ var mergeTrees = function(t1, t2) {
     t1.val += t2.val;
     return t1;
 };
+
+
+
+
+
+/**
+ * 63. 不同路径 II
+ * https://leetcode-cn.com/problems/unique-paths-ii/
+ * 
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    if (obstacleGrid[0][0] == 1) return 0; 
+
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = Array.from(new Array(m), () => new Array(n).fill(0));
+
+    // 第一列
+    for (let i = 0; i < m && obstacleGrid[i][0] === 0; i++) {
+        dp[i][0] = 1;
+    }
+
+    // 第一行
+    for (let j = 0; j < n && obstacleGrid[0][j] === 0; j++) {
+        dp[0][j] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+};
+
+
+
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = Array.from(new Array(m), () => new Array(n));
+
+    // 最右边一排都是 1
+    for (let i = 0; i < m; i++) {
+        dp[i][n - 1] = 1;
+    }
+
+    // 最底下一排都是 1
+    for (let j = 0; j < n; j++) {
+        dp[m - 1][j] = 1;
+    }
+
+    for (let i = m - 2; i >= 0; i--) {
+        for (let j = n - 2; j >= 0; j--) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+    return dp[0][0];
+};
