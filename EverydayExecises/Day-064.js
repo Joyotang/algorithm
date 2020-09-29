087a5a8ab46e23d713e309b311845ffd48d70ef9
TangXiao <tx@vimplus.com>
2020-09-29 21:34:54 +0800

    update day064

--- EverydayExecises/Day-064.js
+++ EverydayExecises/Day-064.js
@@ -20,7 +20,6 @@ var reverseWords = function(s) {
  */
 var reverseOnlyLetters = function(s) {
     let stackLettes = [];
-
     for (let i = 0; i < s.length; i++) {
         if (isLetter(s[i])) {
             stackLettes.push(s[i]);
@@ -42,3 +41,24 @@ function isLetter(s) {
     if (/[a-zA-Z]/.test(s)) return true;
     return false;
 }
+
+
+/* -----------------------------------The next day coding.------------------------------------- */
+var reverseOnlyLetters = function(s) {
+    let stackLettes = [];
+    for (let i = 0; i < s.length; i++) {
+        if (isLetter(s[i])) {
+            stackLettes.push(s[i]);
+        }
+    }
+
+    let strArr = [];
+    for (let i = 0; i < s.length; i++) {
+        if (isLetter(s[i])) {
+            strArr.push(stackLettes.pop());
+        } else {
+            strArr.push(s[i]);
+        }
+    }
+    return strArr.join('');
+};
